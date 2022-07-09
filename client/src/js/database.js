b6aec1080ac457a724be87e7773322890a052834
Dylan <dylan.vonwald@gmail.com>
2022-07-09 16:00:38 -0600

    database.js should be done, working on install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,48 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// PUT/updates text in the database
+export const putDb = async (content) => {
+  console.log("PUT to the database");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const textDb = await openDB("text", 1);
+
+  const tx = textDb.transaction("text", "readwrite");
+
+  const store = tx.objectStore("text");
+
+  const request = store.put({ text: content });
+
+  const result = await request;
+  console.log("Data saved to the database", result);
+};
+
+// GET all text from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  const textDb = await openDB("text", 1);
+
+  const tx = textDb.transaction("text", "readwrite");
+
+  const store = tx.objectStore("text");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
